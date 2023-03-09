d8e696163da869a954b9266bfbeccf3c4d13e957
DrZuse <kirillshelest@gmail.com>
2023-03-09 19:26:02 +0200

    loops

--- multi_backtest.py
+++ multi_backtest.py
@@ -1,5 +1,8 @@
+import datetime
+import numpy as np
 import os
 import pandas as pd
+
 from configurations import setup_logger, basic_parameters as bp
 from multiprocessing import Pool, cpu_count
 
@@ -20,13 +23,13 @@ def read_csv_inparalel(file):
 
 logger.info('start read csvs')
 with Pool(cpu_count()) as p:
-    files = p.map(read_csv_inparalel, theOrderBookFiles)
-    #files = p.map(read_csv_inparalel, theOrderBookFiles[-1:]) # TEST 3 files
+    #files = p.map(read_csv_inparalel, theOrderBookFiles)
+    files = p.map(read_csv_inparalel, theOrderBookFiles[-1:]) # TEST 3 files
 logger.info('finish read csvs')
 
 df = pd.concat(files, ignore_index = True)
 
-df['time UTC'] = pd.to_datetime(df['Open time'], unit='ms', origin='unix')
+#df['time UTC'] = pd.to_datetime(df['Open time'], unit='ms', origin='unix')
 
 logger.info(df.shape)
 
@@ -72,10 +75,88 @@ macd_s = macd.ewm(span=9, adjust=False, min_periods=9).mean()
 # Calculate the difference between the MACD - Trigger for the Convergence/Divergence value
 macd_h = macd - macd_s
 # Add all of our new values for the MACD to the dataframe
-df['macd'] = df.index.map(macd)
+#df['macd'] = df.index.map(macd)
 df['macd_h'] = df.index.map(macd_h)
-df['macd_s'] = df.index.map(macd_s)
+#df['macd_s'] = df.index.map(macd_s)
 
 df.dropna(inplace=True)
 
-logger.info(df.head())
+#logger.info(df.head())
+
+
+df.loc[(df['RSI'] <= 25) & (df['RSI'].shift(1) <= 25), '2RSI<25'] = True
+
+
+logger.info(df.tail(50))
+
+dfnp = np.array(df.values)
+dfnp.shape
+logger.info(dfnp.shape)
+logger.info(dfnp)
+
+wait_rsi25_signal = False
+profits = 0
+losses = 0
+
+for i, dfnp_row_i in enumerate(dfnp):
+    
+    #print(dfnp_row_i)
+    #print(i)
+    if dfnp_row_i[7] == True: # 2RSI<25
+        time_2rsi_lower_25 = dfnp_row_i[0]
+        logger.info(f'2RSI<25: {dfnp_row_i[7]} {datetime.datetime.utcfromtimestamp(time_2rsi_lower_25/1000)}')
+
+        wait_rsi25_signal = True
+
+        for j, dfnp_row_j in enumerate(dfnp[(i+1):]):
+            #print(j)
+            rsi = dfnp_row_j[5]
+            print(f'rsi: {rsi}')
+            if rsi > 25:
+                print('rsi > 25')
+
+                for k, dfnp_row_k in enumerate(dfnp[(i+1)+j:]):
+                    macd = dfnp_row_k[6]
+                    #print(f'macd: {dfnp_row_k[6]}')
+                    if macd >= 0:
+                        print(f'macd: {dfnp_row_k[6]}')
+                        print('buy!')
+
+                        for l, dfnp_row_l in enumerate(dfnp[(i+1)+j+(k+1):]):
+                            print(dfnp_row_l)
+                            print(f'open: {dfnp_row_l[1]} | high: {dfnp_row_l[2]} | low: {dfnp_row_l[3]} | close: {dfnp_row_l[4]}')
+                            buy_price = dfnp_row_l[1]
+                            max_high = dfnp_row_l[2]
+                            max_low = dfnp_row_l[3]
+                            loss = 0
+                            profit = 0
+                            print(f'buy_price: {buy_price}')
+
+                            for m, dfnp_row_m in enumerate(dfnp[(i+1)+j+(k+1)+(l+1):]):
+
+
+                                if dfnp_row_m[3] > max_high:
+                                    max_high = dfnp_row_m[3]
+                                    profit = (max_high - buy_price) * 100 / buy_price
+
+                                if dfnp_row_m[4] < max_low:
+                                    max_low = dfnp_row_m[4]
+                                    loss = (buy_price - max_low) * 100 / buy_price
+
+                                if profit >= 0.02:
+                                    profits += 1
+
+                                if loss >= 0.02:
+                                    losses += 1
+
+                                if profit >= 0.02 or loss >= 0.02:
+                                    print(f'profit: {profit} || loss: {loss}')
+                                    print(f'max_high: {max_high} || max_low: {max_low}')
+                                    print(f'time {datetime.datetime.utcfromtimestamp(dfnp_row_m[0]/1000)}')
+
+                                    break
+                            break
+                        break
+                break
+
+logger.info(f'profits: {profits} || losses: {losses}')
